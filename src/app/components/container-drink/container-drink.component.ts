615315b7a717770a3f7ba7e83c0143b4f1533905
FilipeGabrielRocha <filiperochaprogramador@gmail.com>
2024-03-19 17:40:02 -0300

    término da estilização

--- src/app/components/container-drink/container-drink.component.ts
+++ src/app/components/container-drink/container-drink.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-container-drink',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+  ],
   templateUrl: './container-drink.component.html',
   styleUrl: './container-drink.component.scss'
 })
-export class ContainerDrinkComponent {
+export class ContainerDrinkComponent implements OnInit {
+  @Input()
+  nameDrink:string = ""
+  @Input()
+  altImg:string = ""
+  @Input()
+  ngSrc:string = ""
+  @Input()
+  description:string = ""
+  @Input()
+  price:string = ""
+
+  constructor(){}
+
+  ngOnInit():void {
+  }
 
 }
--- src/app/components/section-drink/section-drink.component.ts
+++ src/app/components/section-drink/section-drink.component.ts
@@ -1,9 +1,14 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { ContainerDrinkComponent } from '../container-drink/container-drink.component';
 
 @Component({
   selector: 'app-section-drink',
   standalone: true,
-  imports: [],
+  imports: [
+    NgOptimizedImage,
+    ContainerDrinkComponent,
+  ],
   templateUrl: './section-drink.component.html',
   styleUrl: './section-drink.component.scss'
 })
